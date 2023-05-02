f5d05e3fb32076320366a98f1738a1609853762c
Igor <moreno.igor.87@gmail.com>
2023-05-02 12:57:02 +0200

    added server and chalk

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
+import { createServer } from "http";
+import chalk from "chalk";
+
 const myString: string = "BikoSchool";
 
-console.log(myString);
+const hostname = "127.0.0.1";
+
+const port: number = 3000;
+
+const server = createServer((req, res) => {
+  console.log(chalk.blue("Peticion realizada"));
+  console.log(res.statusCode);
+  res.end(myString);
+});
+
+server.listen(port, hostname, () => {
+  console.log(chalk.green(`Server running at http://${hostname}:${port}/`));
+});
